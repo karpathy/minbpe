297cf5e3824e07be7f561413da4895b97c0f149f
Arthur Zucker <arthur.zucker@gmail.com>
2024-02-17 11:37:04 +0900

    add option to save / load from disk and from the hub

--- bpe_gpt4.py
+++ bpe_gpt4.py
@@ -4,7 +4,9 @@ Implements the GPT-4 Tokenizer with a light wrapper around the RegexTokenizer.
 
 import tiktoken
 from bpe_regex import RegexTokenizer
-
+import os
+import json
+from transformers.utils import PushToHubMixin, cached_file
 
 def bpe(mergeable_ranks, token, max_rank):
     # helper function used in get_gpt4_merges() to reconstruct the merge forest
@@ -42,18 +44,21 @@ def recover_merges(mergeable_ranks):
 
     return merges
 
-
-class GPT4Tokenizer(RegexTokenizer):
+class GPT4Tokenizer(RegexTokenizer, PushToHubMixin):
     """Lightweight wrapper on RegexTokenizer that matches GPT-4's tokenizer."""
 
-    def __init__(self):
+    def __init__(self, vocab_file=None, merges=None):
         super().__init__()
-        # get the official tokenizer and its merges
-        enc = tiktoken.get_encoding("cl100k_base")
-        mergeable_ranks = enc._mergeable_ranks
-        # the merges are those of gpt4, but we have to recover them
-        self.merges = recover_merges(mergeable_ranks)
-        # reconstruct the vocab from the merges
+        if merges is None:
+            # get the official tokenizer and its merges
+            enc = tiktoken.get_encoding("cl100k_base")
+            self.mergeable_ranks = enc._mergeable_ranks
+            # the merges are those of gpt4, but we have to recover them
+        else:
+            with open(vocab_file,  encoding="utf-8") as vocab_handle:
+                self.mergeable_ranks = json.load(vocab_handle)
+
+        self.merges = recover_merges(self.mergeable_ranks)
         vocab = {idx: bytes([idx]) for idx in range(256)}
         for (p0, p1), idx in self.merges.items():
             vocab[idx] = vocab[p0] + vocab[p1]
@@ -62,7 +67,7 @@ class GPT4Tokenizer(RegexTokenizer):
         # for some reason, the tokens corresponding to individual bytes
         # are permuted in a different order. This is completely non-sensical
         # and probably historical, but therefore we have to deal with it here.
-        self.byte_shuffle = {i: mergeable_ranks[bytes([i])] for i in range(256)}
+        self.byte_shuffle = {i: self.mergeable_ranks[bytes([i])] for i in range(256)}
         self.inverse_byte_shuffle = {v: k for k, v in self.byte_shuffle.items()}
 
     def _encode_chunk(self, text_bytes):
@@ -77,6 +82,32 @@ class GPT4Tokenizer(RegexTokenizer):
         text_bytes = bytes(self.inverse_byte_shuffle[b] for b in text_bytes)
         text = text_bytes.decode("utf-8", errors="replace")
         return text
+    
+    def save_pretrained(self, save_directory, **kwargs):
+        os.makedirs(save_directory, exist_ok=True)
+        vocab_file = os.path.join(save_directory, "vocab.txt")
+        # we usually save the merges in a string format with token.decode(erros="replace") but let's  be simple
+        with open(vocab_file, "w",encoding="utf-8") as writer:
+            for token, _ in sorted(self.mergeable_ranks.items(), key=lambda kv: kv[1]):
+                writer.write(token.decode("utf-8", errors="replace")+"\n")
+        return vocab_file
+    
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name=None, **kwargs):
+        resolved_vocab_files = {"vocab_file":"vocab.txt",}
+        for file_id, file_path in resolved_vocab_files.items():
+            resolved_vocab_files[file_id] = cached_file(pretrained_model_name, file_path, **kwargs)
+
+        # Instantiate the tokenizer.
+        try:
+            tokenizer = GPT4Tokenizer(**resolved_vocab_files)
+        except OSError:
+            raise OSError(
+                "Unable to load vocabulary from file. "
+                "Please check that the provided vocabulary is accessible and not corrupted."
+            )
+        return tokenizer
+        
 
 if __name__ == "__main__":
     # let's take it for a spin!
@@ -86,6 +117,9 @@ if __name__ == "__main__":
     # vs.
     tokenizer = GPT4Tokenizer()
     # fight!
+    
+    tokenizer.push_to_hub("ArthurZ/gpt-min", private=True)
+    tokenizer = GPT4Tokenizer.from_pretrained("ArthurZ/gpt-min")
 
     text = "hello world!!!? (안녕하세요!) lol123 😉"
     print(text)
