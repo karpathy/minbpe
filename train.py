a04b85accaf34190fdc5d2f966923fde3edd2fa0
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-02-17 17:14:42 -0800

    let's not clutter the main directory

--- train.py
+++ train.py
@@ -3,15 +3,20 @@ Train our Tokenizers on some data, just to see them in action.
 The whole thing runs in ~25 seconds on my laptop.
 """
 
+import os
 from minbpe import BasicTokenizer, RegexTokenizer
 
 # open some text and train a vocab of 512 tokens
 text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
+
+# create a directory for models so we don't pollute the current directory
+os.makedirs("models", exist_ok=True)
 
 for TokenizerClass, name in zip([BasicTokenizer, RegexTokenizer], ["basic", "regex"]):
 
     # construct the Tokenizer object and kick off verbose training
     tokenizer = TokenizerClass()
     tokenizer.train(text, 512, verbose=True)
-    # writes two files in current directory: name.model, and name.vocab
-    tokenizer.save(name)
+    # writes two files in the models directory: name.model, and name.vocab
+    prefix = os.path.join("models", name)
+    tokenizer.save(prefix)
