a5ecf46e568fc77c39aae53ca47dcb7ba5fb4c53
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-02-20 00:25:44 +0900

    Update regex.py
    occurence -> occurrence

--- minbpe/regex.py
+++ minbpe/regex.py
@@ -134,7 +134,7 @@ class RegexTokenizer(Tokenizer):
             return self.encode_ordinary(text)
         # otherwise, we have to be careful with potential special tokens in text
         # we handle special tokens by splitting the text
-        # based on the occurence of any exact match with any of the special tokens
+        # based on the occurrence of any exact match with any of the special tokens
         # we can use re.split for this. note that surrounding the pattern with ()
         # makes it into a capturing group, so the special tokens will be included
         special_pattern = "(" + "|".join(re.escape(k) for k in special) + ")"
