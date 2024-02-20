c52320affbd21e0c587281ddfcf29d1edf6e857f
nobleaustine <nobleaustine2001@gmail.com>
2024-02-20 20:37:18 +0530

    changed test_strings into a function decorated with @pytest.fixtures

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -6,14 +6,16 @@ from minbpe import BasicTokenizer, RegexTokenizer, GPT4Tokenizer
 
 # -----------------------------------------------------------------------------
 # common test data
+@pytest.fixture
+def test_strings():
+    # a few strings to test the tokenizers on
+    return [
+        "", # empty string
+        "?", # single character
+        "hello world!!!? (안녕하세요!) lol123 😉", # fun small string
+        "FILE:taylorswift.txt", # FILE: is handled as a special string in unpack()
+    ]
 
-# a few strings to test the tokenizers on
-test_strings = [
-    "", # empty string
-    "?", # single character
-    "hello world!!!? (안녕하세요!) lol123 😉", # fun small string
-    "FILE:taylorswift.txt", # FILE: is handled as a special string in unpack()
-]
 def unpack(text):
     # we do this because `pytest -v .` prints the arguments to console, and we don't
     # want to print the entire contents of the file, it creates a mess. So here we go.
@@ -50,23 +52,23 @@ The ancestors of llamas are thought to have originated from the Great Plains of 
 
 # test encode/decode identity for a few different strings
 @pytest.mark.parametrize("tokenizer_factory", [BasicTokenizer, RegexTokenizer, GPT4Tokenizer])
-@pytest.mark.parametrize("text", test_strings)
-def test_encode_decode_identity(tokenizer_factory, text):
-    text = unpack(text)
-    tokenizer = tokenizer_factory()
-    ids = tokenizer.encode(text)
-    decoded = tokenizer.decode(ids)
-    assert text == decoded
+def test_encode_decode_identity(tokenizer_factory, test_strings):
+    for text in test_strings:
+        text = unpack(text)
+        tokenizer = tokenizer_factory()
+        ids = tokenizer.encode(text)
+        decoded = tokenizer.decode(ids)
+        assert text == decoded
 
 # test that our tokenizer matches the official GPT-4 tokenizer
-@pytest.mark.parametrize("text", test_strings)
-def test_gpt4_tiktoken_equality(text):
-    text = unpack(text)
-    tokenizer = GPT4Tokenizer()
-    enc = tiktoken.get_encoding("cl100k_base")
-    tiktoken_ids = enc.encode(text)
-    gpt4_tokenizer_ids = tokenizer.encode(text)
-    assert gpt4_tokenizer_ids == tiktoken_ids
+def test_gpt4_tiktoken_equality(test_strings):
+    for text in test_strings:
+        text = unpack(text)
+        tokenizer = GPT4Tokenizer()
+        enc = tiktoken.get_encoding("cl100k_base")
+        tiktoken_ids = enc.encode(text)
+        gpt4_tokenizer_ids = tokenizer.encode(text)
+        assert gpt4_tokenizer_ids == tiktoken_ids
 
 # test the handling of special tokens
 def test_gpt4_tiktoken_equality_special_tokens():
