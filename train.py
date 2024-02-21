f12106f927bd567cb14ab77ecbdb11af9b31d411
Jason Liu <jason@jxnl.co>
2024-02-20 20:32:02 -0500

    update timing

--- train.py
+++ train.py
@@ -17,11 +17,11 @@ for TokenizerClass, name in zip([BasicTokenizer, RegexTokenizer], ["basic", "reg
     # construct the Tokenizer object and kick off verbose training
     t0 = time.perf_counter()
     tokenizer = TokenizerClass()
-    tokenizer.train(text, 512, verbose=True)
+    tokenizer.train(text, 512, verbose=False)
     # writes two files in the models directory: name.model, and name.vocab
     prefix = os.path.join("models", name)
     tokenizer.save(prefix)
     t1 = time.perf_counter()
     print(f"Training {name} tokenizer took {t1 - t0:.2f} seconds")
-    # > Training basic tokenizer took 3.56 seconds
-    # > Training regex tokenizer took 9.14 seconds
+    # > Training basic tokenizer took 3.46 seconds
+    # > Training regex tokenizer took 8.84 seconds
