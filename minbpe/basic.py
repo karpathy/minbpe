eb7d3a1cfce3a39b900bade1c4e5d63fe9baa983
laihongphuc <phuckstnk63dktd@gmail.com>
2024-02-21 15:05:40 +0700

    Simplify encode function

--- minbpe/basic.py
+++ minbpe/basic.py
@@ -54,21 +54,32 @@ class BasicTokenizer(Tokenizer):
         text = text_bytes.decode("utf-8", errors="replace")
         return text
 
+    # def encode(self, text):
+    #     # given a string text, return the token ids
+    #     text_bytes = text.encode("utf-8") # raw bytes
+    #     ids = list(text_bytes) # list of integers in range 0..255
+    #     while len(ids) >= 2:
+    #         # find the pair with the lowest merge index
+    #         stats = get_stats(ids)
+    #         pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
+    #         # subtle: if there are no more merges available, the key will
+    #         # result in an inf for every single pair, and the min will be
+    #         # just the first pair in the list, arbitrarily
+    #         # we can detect this terminating case by a membership check
+    #         if pair not in self.merges:
+    #             break # nothing else can be merged anymore
+    #         # otherwise let's merge the best pair (lowest merge index)
+    #         idx = self.merges[pair]
+    #         ids = merge(ids, pair, idx)
+    #     return ids
+
     def encode(self, text):
         # given a string text, return the token ids
         text_bytes = text.encode("utf-8") # raw bytes
         ids = list(text_bytes) # list of integers in range 0..255
-        while len(ids) >= 2:
-            # find the pair with the lowest merge index
-            stats = get_stats(ids)
-            pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
-            # subtle: if there are no more merges available, the key will
-            # result in an inf for every single pair, and the min will be
-            # just the first pair in the list, arbitrarily
-            # we can detect this terminating case by a membership check
-            if pair not in self.merges:
-                break # nothing else can be merged anymore
-            # otherwise let's merge the best pair (lowest merge index)
-            idx = self.merges[pair]
+        # make a list of (new index, merged pair) in the order in which they are merged
+        s2p = sorted([(v, k) for k, v in self.merges.items()])
+        # let's merge 
+        for idx, pair in s2p:
             ids = merge(ids, pair, idx)
         return ids
