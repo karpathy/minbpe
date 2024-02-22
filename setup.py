7863a612a6188d39be479dc0558fc00a1ca683e1
Biswajit Satapathy <biswajit2902@gmail.com>
2024-02-22 15:10:11 +0530

    Create setup.py
    This is to facilitate installation.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as fp:
+    requires = [i.strip() for i in  fp.readlines() if i[0] != "#"]
+
+setup(
+    name='minimal-bpe',
+    version='0.0.0', # Version can be maintained separately
+    description='A library for Byte Pair Encoding experiments',
+    author='Andrej Karpathy',
+    author_email='andrej.karpathy@gmail.com',
+    packages=["minbpe"],
+    install_requires=requires
+)
