81e0599a3442c99aa6c7b003f6e184e8d37ff009
Brett Kuprel <brkuprel@gmail.com>
2024-02-21 17:05:49 -0800

    train_basic

---
+++ train_basic.py
@@ -0,0 +1,27 @@
+"""
+Train our Tokenizers on some data, just to see them in action.
+The whole thing runs in ~25 seconds on my laptop.
+"""
+
+import os
+import time
+from minbpe import BasicTokenizer
+
+# open some text and train a vocab of 512 tokens
+text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
+
+# create a directory for models, so we don't pollute the current directory
+os.makedirs("models", exist_ok=True)
+
+t0 = time.time()
+
+# construct the Tokenizer object and kick off verbose training
+tokenizer = BasicTokenizer()
+tokenizer.train(text, 512, verbose=True)
+# writes two files in the models directory: name.model, and name.vocab
+prefix = os.path.join("models", "basic")
+tokenizer.save(prefix)
+
+t1 = time.time()
+
+print(f"Training took {t1 - t0:.2f} seconds")
