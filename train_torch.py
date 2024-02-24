a23fcc33fb793bda5a1f365184d93a61cb7e51f6
Brett Kuprel <brkuprel@gmail.com>
2024-02-24 15:15:22 -0800

    make train_torch.py near identical to train.py

--- train_torch.py
+++ train_torch.py
@@ -1,11 +1,10 @@
 """
-Train BasicTokenizer on some data using the GPU
+Train torch Tokenizers on some data
 """
 
 import os
 import time
-import torch
-from minbpe import BasicTokenizerTorch
+from minbpe import BasicTokenizerTorch, RegexTokenizerTorch
 
 # open some text and train a vocab of 512 tokens
 text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
@@ -14,22 +13,14 @@ text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
 os.makedirs("models", exist_ok=True)
 
 t0 = time.time()
+for TokenizerClass, name in zip([BasicTokenizerTorch, RegexTokenizerTorch], ["basic", "regex"]):
 
-# construct the Tokenizer object and kick off verbose training
-tokenizer = BasicTokenizerTorch()
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Training with {device}")
-tokenizer.train(text, 512, verbose=True, device=device)
-# writes two files in the models directory: name.model, and name.vocab
-prefix = os.path.join("models", "basic")
-tokenizer.save(prefix)
-
+    # construct the Tokenizer object and kick off verbose training
+    tokenizer = TokenizerClass()
+    tokenizer.train(text, 512, verbose=True)
+    # writes two files in the models directory: name.model, and name.vocab
+    prefix = os.path.join("models", name)
+    tokenizer.save(prefix)
 t1 = time.time()
 
 print(f"Training took {t1 - t0:.2f} seconds")
-
-print("Testing the model")
-tok = BasicTokenizerTorch()
-tok.load(prefix + ".model")
-assert(tok.decode(tok.encode(text)) == text)
-print("Success")
