a87ed6b8324c5360f6f6bed7dc7b15ae70774081
Brett Kuprel <brkuprel@gmail.com>
2024-02-24 19:30:35 -0800

    only train BasicTokenizerTorch in train_torch.py

--- train_torch.py
+++ train_torch.py
@@ -1,10 +1,10 @@
 """
-Train torch tokenizers on some data
+Train torch tokenizer on some data
 """
 
 import os
 import time
-from minbpe import BasicTokenizerTorch, RegexTokenizerTorch
+from minbpe import BasicTokenizerTorch
 
 # open some text and train a vocab of 512 tokens
 text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
@@ -13,14 +13,12 @@ text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
 os.makedirs("models", exist_ok=True)
 
 t0 = time.time()
-for TokenizerClass, name in zip([BasicTokenizerTorch, RegexTokenizerTorch], ["basic", "regex"]):
-
-    # construct the Tokenizer object and kick off verbose training
-    tokenizer = TokenizerClass()
-    tokenizer.train(text, 512, verbose=True)
-    # writes two files in the models directory: name.model, and name.vocab
-    prefix = os.path.join("models", name)
-    tokenizer.save(prefix)
+# construct the Tokenizer object and kick off verbose training
+tokenizer = BasicTokenizerTorch()
+tokenizer.train(text, 512, verbose=True)
+# writes two files in the models directory: name.model, and name.vocab
+prefix = os.path.join("models", "basic")
+tokenizer.save(prefix)
 t1 = time.time()
 
 print(f"Training took {t1 - t0:.2f} seconds")
