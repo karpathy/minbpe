aa6839b4538613991f3a9df8163ca55642db7b4a
Ruhollah Majdoddin <r.majdodin@gmail.com>
2024-06-08 14:23:22 +0200

    in train(), just one instance of each string in ids, keep count of its occurences in idsw

--- minbpe/base.py
+++ minbpe/base.py
@@ -10,7 +10,7 @@ import unicodedata
 # -----------------------------------------------------------------------------
 # a few helper functions useful for both BasicTokenizer and RegexTokenizer
 
-def get_stats(ids, counts=None):
+def get_stats(ids, counts=None, weight=1):
     """
     Given a list of integers, return a dictionary of counts of consecutive pairs
     Example: [1, 2, 3, 1, 2] -> {(1, 2): 2, (2, 3): 1, (3, 1): 1}
@@ -18,7 +18,7 @@ def get_stats(ids, counts=None):
     """
     counts = {} if counts is None else counts
     for pair in zip(ids, ids[1:]): # iterate consecutive elements
-        counts[pair] = counts.get(pair, 0) + 1
+        counts[pair] = counts.get(pair, 0) + weight
     return counts
 
 
--- minbpe/regex.py
+++ minbpe/regex.py
@@ -41,7 +41,16 @@ class RegexTokenizer(Tokenizer):
         text_chunks = re.findall(self.compiled_pattern, text)
 
         # input text preprocessing
-        ids = [list(ch.encode("utf-8")) for ch in text_chunks]
+        ids = (list(ch.encode("utf-8")) for ch in text_chunks)
+
+        #keep just one instance of identical chunks, keep their count in idsw
+        tmp = {}
+        for byte_str in ids:
+            byte_str = bytes(byte_str)
+            tmp[byte_str] = tmp[byte_str] + 1 if byte_str in tmp else 1
+
+        ids = [list(k) for k in map(list, tmp.keys())]
+        idsw = list(tmp.values())
 
         # iteratively merge the most common pairs to create new tokens
         merges = {} # (int, int) -> int
@@ -49,9 +58,9 @@ class RegexTokenizer(Tokenizer):
         for i in range(num_merges):
             # count the number of times every consecutive pair appears
             stats = {}
-            for chunk_ids in ids:
+            for j, chunk_ids in enumerate(ids):
                 # passing in stats will update it in place, adding up counts
-                get_stats(chunk_ids, stats)
+                get_stats(chunk_ids, stats, idsw[j])
             # find the pair with the highest count
             pair = max(stats, key=stats.get)
             # mint a new token: assign it the next available id
